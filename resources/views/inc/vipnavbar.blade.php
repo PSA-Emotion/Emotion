3f19c30965cfe1faf6321d4f7376c59a8e0e2e98
EdvinasZel <49122820+EdvinasZel@users.noreply.github.com>
2021-05-13 12:06:27 +0300

    sdsd

--- resources/views/inc/vipnavbar.blade.php
+++ resources/views/inc/vipnavbar.blade.php
@@ -1,3 +1,4 @@
+
 <nav class="navbar navbar-expand-md navbar-light shadow-sm" style="background-color: #a2f7a4">
     <div class="container">
         <p class="navbar-brand">
@@ -15,7 +16,7 @@
 
             @auth
                 <ul class="nav nav-pills">
-                    <li class="nav-item"><a href="/dashboard" class="nav-link">Pagrindinis langas</a></li>
+                    <li class="nav-item"><a href="/dashboard" class="nav-link btn btn-success">Pagrindinis langas</a></li>
                     <li class="nav-item"><a href="/vip" class="nav-link">Moderatoriaus langas</a></li>
                     <li class="nav-item"><a href="/vipMutes" class="nav-link">Atitildymo prašymai</a></li>
                     <li class="nav-item"><a href="/vipBans" class="nav-link">Atblokavimo prašymai</a></li>
@@ -30,6 +31,16 @@
             <ul class="navbar-nav ml-auto">
                 <!-- Authentication Links -->
 
+                @if(auth()->user()->status == 'admin')
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                    </svg>
+                @elseif(auth()->user()->status == 'vip')
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-award-fill" viewBox="0 0 16 16">
+                        <path d="m8 0 1.669.864 1.858.282.842 1.68 1.337 1.32L13.4 6l.306 1.854-1.337 1.32-.842 1.68-1.858.282L8 12l-1.669-.864-1.858-.282-.842-1.68-1.337-1.32L2.6 6l-.306-1.854 1.337-1.32.842-1.68L6.331.864 8 0z"/>
+                        <path d="M4 11.794V16l4-1 4 1v-4.206l-2.018.306L8 13.126 6.018 12.1 4 11.794z"/>
+                    </svg>
+                @endif
                 <img style="width:50px; height:50px; vertical-align: middle; border-radius: 50% " src="/storage/profile_images/{{auth()->user()->profile_image}}">
                 <li class="nav-item dropdown">
                     <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,7 +2,18 @@
 
 @section('content')
     <a href="/posts" class="btn btn-lg btn-block btn-outline-primary">Grįžti</a>
-    <h1 class="text-center">{{$post->title}}</h1>
+    @if($post->user->status == 'admin')
+    <h1 class="text-center">{{$post->title}} <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+        </svg></h1>
+    @elseif($post->user->status == 'vip')
+        <h1 class="text-center">{{$post->title}} <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-award-fill" viewBox="0 0 16 16">
+                <path d="m8 0 1.669.864 1.858.282.842 1.68 1.337 1.32L13.4 6l.306 1.854-1.337 1.32-.842 1.68-1.858.282L8 12l-1.669-.864-1.858-.282-.842-1.68-1.337-1.32L2.6 6l-.306-1.854 1.337-1.32.842-1.68L6.331.864 8 0z"/>
+                <path d="M4 11.794V16l4-1 4 1v-4.206l-2.018.306L8 13.126 6.018 12.1 4 11.794z"/>
+            </svg></h1>
+    @else
+        <h1 class="text-center">{{$post->title}}</h1>
+        @endif
 
     <img style="width:20%;" class="text-center" src="/storage/cover_images/{{$post->cover_image}}">
     <br>
